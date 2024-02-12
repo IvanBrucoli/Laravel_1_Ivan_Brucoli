6ee83ccc574a236270b46b96d6cc6066dda1f809
Ivan Brucoli <ivan23197@gmail.com>
2024-02-12 15:37:03 +0100

    esercizio completato

---
+++ Ivan_Brucoli_Primo_Progetto/resources/views/pagina_1.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Pagina 1</h1>
+
+    <nav class="navbar bg-info border-bottom border-body">
+      <ul>
+        <li class="nav-item"><a class=nav-link href="/Home">Home</a></li>
+        <li class="nav-item"><a class=nav-link href="/pagina_1">Pagina 1</a></li>
+        <li class="nav-item"><a class=nav-link href="/pagina_2">Pagina 2</a></li>
+      </ul>
+</nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
